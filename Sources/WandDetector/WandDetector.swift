28144f66901658a8bc58cffd72253ae1fb5fcdf0
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2020-07-27 17:07:47 -0400

    implement methods for filtration and color cube creation

--- Sources/WandDetector/WandDetector.swift
+++ Sources/WandDetector/WandDetector.swift
@@ -1,15 +1,20 @@
 import CoreImage
+import UIKit.UIColor
 import CoreImage.CIFilterBuiltins
 
 public typealias ImageSize = (width: Int, height: Int)
 public typealias Wand = (center: (x: Double, y: Double), radius: Double)
 public typealias ImageRegion = (origin: (x: Int, y: Int), size: ImageSize)
 
+private typealias ColorCube = (data: Data, dimension: Float)
+
 public enum WandDetectorError: Error {
     case invalidWand
     case invalidImage
+    case couldNotUseFilter
     case invalidRegionOfInterest
     case invalidMaxRetainedOutputImages
+    case couldNotCreatePixelBuffer(code: CVReturn)
     case couldNotCreatePixelBufferPool(code: CVReturn)
 }
 
@@ -18,7 +23,7 @@ public struct WandDetector {
     // screen projection size at some standard gameplay distance for that screen
     private let minPixels = 50_000
 
-    private let roi: ImageRegion
+    private let roiRect: CGRect
     private let context: CIContext
     private let inputSize: ImageSize
     private let pool: CVPixelBufferPool
@@ -41,11 +46,11 @@ public struct WandDetector {
         }
 
         // make CGRects from inputSize and roi
-        let inputRect = CGRect(origin: CGPoint.zero, size: CGSize(width: inputSize.width, height: inputSize.height))
+        let inputSizeRect = CGRect(origin: CGPoint.zero, size: CGSize(width: inputSize.width, height: inputSize.height))
         let roiRect = CGRect(origin: CGPoint(x: roi.origin.x, y: roi.origin.y), size: CGSize(width: roi.size.width, height: roi.size.height))
 
-        // verify inputRect contains roiRect
-        guard inputRect.contains(roiRect) else {
+        // verify inputSizeRect contains roiRect
+        guard inputSizeRect.contains(roiRect) else {
             throw WandDetectorError.invalidRegionOfInterest
         }
 
@@ -125,9 +130,9 @@ public struct WandDetector {
         thresholdFilter.colorSpace = context.workingColorSpace
 
         // initialize stored properties
-        self.roi = roi
         self.pool = pool
         self.context = context
+        self.roiRect = roiRect
         self.inputSize = inputSize
         self.transform = transform
         self.erosionFilter = erosionFilter
@@ -137,7 +142,7 @@ public struct WandDetector {
 
     // MARK: Calibration
 
-    public func calibrate(using wand: Wand, inRegionOfInterestIn image: CVImageBuffer) throws -> CVImageBuffer {
+    public func calibrate(using wand: Wand, inRegionOfInterestIn image: CVImageBuffer) throws {
         // verify image size is correct
         let width = CVPixelBufferGetWidth(image)
         let height = CVPixelBufferGetHeight(image)
@@ -145,23 +150,134 @@ public struct WandDetector {
             throw WandDetectorError.invalidImage
         }
 
-        // make CGRects from roi and wand
-        let roiRect = CGRect(origin: CGPoint(x: self.roi.origin.x, y: self.roi.origin.y), size: CGSize(width: self.roi.size.width, height: self.roi.size.height))
+        // make CGRect from wand
         let diameter = wand.radius * 2
         var wandRect = CGRect(origin: CGPoint(x: wand.center.x - wand.radius, y: wand.center.y - wand.radius), size: CGSize(width: diameter, height: diameter))
 
         assert(wandRect.midX == CGFloat(wand.center.x) && wandRect.midY == CGFloat(wand.center.y), "wandRect is centered incorrectly")
 
         // verify roiRect contains wandRect
-        guard roiRect.contains(wandRect) else {
+        guard self.roiRect.contains(wandRect) else {
             throw WandDetectorError.invalidWand
         }
 
         // apply the transform to wandRect
         wandRect = wandRect.applying(self.transform)
 
+
         // begin binary search of optimal filter parameters
-        //
-        //
+
+        var minH: CGFloat = 0, maxH: CGFloat = 1
+        var minS: CGFloat = 0, maxS: CGFloat = 1
+        var minB: CGFloat = 0, maxB: CGFloat = 1
+
+        let colorCube = self.createColorCube(hueRange: minH...maxH, saturationRange: minS...maxS, brightnessRange: minB...maxB)
+        thresholdFilter.cubeData = colorCube.data
+        thresholdFilter.cubeDimension = colorCube.dimension
+
+        let filtered = try self.filter(image: image)
     }
+
+    // MARK: Filtration
+
+    private func filter(image: CVImageBuffer) throws -> CVImageBuffer {
+        // crop
+        let cropped = CIImage(cvImageBuffer: image).cropped(to: self.roiRect)
+
+        // transform
+        let transformed = cropped.transformed(by: self.transform)
+
+        // threshold
+        self.thresholdFilter.inputImage = transformed
+        guard let thresholded = self.thresholdFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+
+        // clamp
+        let clamped = thresholded.clampedToExtent()
+
+        // dilate
+        self.dilationFilter.inputImage = clamped
+        guard let dilated = self.dilationFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+
+        // erode thrice
+        self.erosionFilter.inputImage = dilated
+        guard let eroded = self.erosionFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+        self.erosionFilter.inputImage = eroded
+        guard let erodedx2 = self.erosionFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+        self.erosionFilter.inputImage = erodedx2
+        guard let erodedx3 = self.erosionFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+
+        // dilate twice
+        self.dilationFilter.inputImage = erodedx3
+        guard let dilatedx2 = self.dilationFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+        self.dilationFilter.inputImage = dilatedx2
+        guard let filtered = self.dilationFilter.outputImage else {
+            throw WandDetectorError.couldNotUseFilter
+        }
+
+        // create the output pixel buffer
+        var pixelBufferOut: CVPixelBuffer?
+        let code = CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, self.pool, &pixelBufferOut)
+        guard let output = pixelBufferOut else {
+            throw WandDetectorError.couldNotCreatePixelBuffer(code: code)
+        }
+
+        // render to the output
+        self.context.render(filtered, to: output)
+
+        return output
+    }
+
+    // MARK: Color Cube Creation
+
+      private func createColorCube(hueRange: ClosedRange<CGFloat>, saturationRange: ClosedRange<CGFloat>, brightnessRange: ClosedRange<CGFloat>) -> ColorCube {
+          var cube = [Float]()
+
+          let size = 64
+          let dimension = Float(size)
+
+          for z in 0 ..< size {
+              let blue = CGFloat(z) / CGFloat(size-1)
+
+              for y in 0 ..< size {
+                  let green = CGFloat(y) / CGFloat(size-1)
+
+                  for x in 0 ..< size {
+                      let red = CGFloat(x) / CGFloat(size-1)
+
+                      var hue: CGFloat = 0, saturation: CGFloat = 0, brightness: CGFloat = 0
+                      let rgba = UIColor(red: red, green: green, blue: blue, alpha: 1)
+                      rgba.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
+
+                      let color: Float =
+                          hueRange.contains(hue) &&
+                          saturationRange.contains(saturation) &&
+                          brightnessRange.contains(brightness) ? 1 : 0
+
+                      cube.append(color) // red
+                      cube.append(color) // green
+                      cube.append(color) // blue
+                      cube.append(color) // alpha
+                  }
+              }
+          }
+
+          var data = Data()
+          cube.withUnsafeBufferPointer({ buffer in
+              data.append(buffer)
+          })
+
+          return (data: data, dimension: dimension)
+      }
 }
